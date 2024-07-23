0b0319395235b15904e693dac7cbe94a811a8c8f
daffa tabiano <daffatabianoo@gmail.com>
2024-07-24 06:46:59 +0700

    feat(modals) : setup modals

--- src/views/MainViews.tsx
+++ src/views/MainViews.tsx
@@ -1,3 +1,10 @@
+import {
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalTrigger,
+} from '@/components/ui/modals';
 import useGet from '@/hooks/useGet';
 import { BentoGridItem } from '@/layouts/partials/BentoGrid';
 import {
@@ -9,61 +16,25 @@ import {
     IconSignature,
     IconTableColumn,
 } from '@tabler/icons-react';
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
 const Skeleton = () => (
     <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
 );
-const items = [
-    {
-        title: 'The Dawn of Innovation',
-        description:
-            'Explore the birth of groundbreaking ideas and inventions.',
-        header: <Skeleton />,
-        icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-        title: 'The Digital Revolution',
-        description: 'Dive into the transformative power of technology.',
-        header: <Skeleton />,
-        icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-        title: 'The Art of Design',
-        description: 'Discover the beauty of thoughtful and functional design.',
-        header: <Skeleton />,
-        icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-        title: 'The Power of Communication',
-        description:
-            'Understand the impact of effective communication in our lives.',
-        header: <Skeleton />,
-        icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-        title: 'The Pursuit of Knowledge',
-        description: 'Join the quest for understanding and enlightenment.',
-        header: <Skeleton />,
-        icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-        title: 'The Joy of Creation',
-        description: 'Experience the thrill of bringing ideas to life.',
-        header: <Skeleton />,
-        icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
-    },
-    {
-        title: 'The Spirit of Adventure',
-        description: 'Embark on exciting journeys and thrilling discoveries.',
-        header: <Skeleton />,
-        icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
-    },
-];
 
 export default function MainViews() {
     const { get } = useGet();
     const [isNews, setIsNews] = useState([]);
+
+    const images = [
+        'https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1573790387438-4da905039392?q=80&w=3425&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1555400038-63f5ba517a47?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1554931670-4ebfabf6e7a9?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        'https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=2581&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ];
 
     const getData = async () => {
         const res = await get(
@@ -72,19 +43,79 @@ export default function MainViews() {
         setIsNews(res?.data?.articles);
     };
 
-    console.log(isNews);
-
     useEffect(() => {
         getData();
     }, []);
 
     return (
         <>
+            <div className="py-40  flex items-center justify-center">
+                <Modal>
+                    <ModalTrigger className="bg-black dark:bg-white dark:text-black text-white flex justify-center group/modal-btn">
+                        <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500">
+                            Book your flight
+                        </span>
+                        <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
+                            ✈️
+                        </div>
+                    </ModalTrigger>
+                    <ModalBody>
+                        <ModalContent>
+                            <h4 className="text-lg md:text-2xl text-neutral-600 dark:text-neutral-100 font-bold text-center mb-8">
+                                Book your trip to{' '}
+                                <span className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200">
+                                    Bali
+                                </span>{' '}
+                                now! ✈️
+                            </h4>
+                            <div className="flex justify-center items-center">
+                                {images.map((image, idx) => (
+                                    <motion.div
+                                        key={'images' + idx}
+                                        style={{
+                                            rotate: Math.random() * 20 - 10,
+                                        }}
+                                        whileHover={{
+                                            scale: 1.1,
+                                            rotate: 0,
+                                            zIndex: 100,
+                                        }}
+                                        whileTap={{
+                                            scale: 1.1,
+                                            rotate: 0,
+                                            zIndex: 100,
+                                        }}
+                                        className="rounded-xl -mr-4 mt-4 p-1 bg-white dark:bg-neutral-800 dark:border-neutral-700 border border-neutral-100 flex-shrink-0 overflow-hidden"
+                                    >
+                                        <Image
+                                            src={image}
+                                            alt="bali images"
+                                            width="500"
+                                            height="500"
+                                            className="rounded-lg h-20 w-20 md:h-40 md:w-40 object-cover flex-shrink-0"
+                                        />
+                                    </motion.div>
+                                ))}
+                            </div>
+                            <div className="py-10 flex flex-wrap gap-x-4 gap-y-6 items-start justify-start max-w-sm mx-auto">
+                                hello world
+                            </div>
+                        </ModalContent>
+                        <ModalFooter className="gap-4">
+                            <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
+                                Cancel
+                            </button>
+                            <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
+                                Book Now
+                            </button>
+                        </ModalFooter>
+                    </ModalBody>
+                </Modal>
+            </div>
             <BentoGridItem
                 title={isNews[0]?.title}
                 description={isNews[0]?.description}
                 header={<img src={isNews[0]?.urlToImage} />}
-                icon={items[0].icon}
                 className={'md:row-span-full'}
             />
         </>
